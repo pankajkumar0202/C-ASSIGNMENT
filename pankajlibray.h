8610c38d8fd052163a2be85f6c82f09cf4ce1956
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2025-10-21 13:04:42 +0530

    pankajlibray.h

---
+++ pankajlibray.h
@@ -0,0 +1,10 @@
+#ifndef PANKAJ_H
+#define PANKAJ_H
+
+void displayArray(int arr[], int size);
+void reverseArray(int arr[], int size);
+int findMaxIndex(int arr[], int size);
+int findMinIndex(int arr[], int size);
+float findAverage(int arr[], int size);
+int linearSearch(int arr[], int size, int value);
+#endif 
