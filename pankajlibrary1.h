23b759c290c3db718ef0f1c98de20b8c5eca3d43
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2025-10-21 13:16:14 +0530

    pankajlibrary1.h

---
+++ pankajlibrary1.h
@@ -0,0 +1,8 @@
+#ifndef PANKAJ_H
+#define PANKAJ_H
+// Number functions
+int reverseDigits(int n);
+int isArmstrong(int num);
+int isAdams(int num);
+int isPrime(int num);
+int isPrimePalindrome(int num);
